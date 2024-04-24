66bec28253b82c5486814b8d55266df3e3e70670
don <donaldolivergavino@gmail.com>
2024-04-24 22:54:02 +0800

    change server host

--- server/index.js
+++ server/index.js
@@ -10,13 +10,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 // Set CORS headers for specific routes
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://marketplace-frontend-blue.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', true);
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'https://marketplace-frontend-blue.vercel.app');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.header('Access-Control-Allow-Credentials', true);
+//     next();
+// });
 
 
 // Routes
