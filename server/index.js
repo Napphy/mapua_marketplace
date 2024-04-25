0ee14c723c5454d7cd90a11a7edecb823c062ffe
Napphy <dosgavinojr@mymail.mapua.edu.ph>
2024-04-25 13:33:28 +0800

    Revert "change server host"
    This reverts commit 66bec28253b82c5486814b8d55266df3e3e70670.

--- server/index.js
+++ server/index.js
@@ -10,6 +10,17 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
+<<<<<<< HEAD
+=======
+// Set CORS headers for specific routes
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://marketplace-frontend-blue.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
+>>>>>>> parent of 66bec28 (change server host)
 
 
 // Routes
