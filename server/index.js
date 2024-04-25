4c331ece939cb1adbfc3655e23e36d9f7742f6b4
Napphy <dosgavinojr@mymail.mapua.edu.ph>
2024-04-25 13:47:16 +0800

    remove allowCors

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRouter = require('./routes/authRoutes');
-const allowCors = require('./utils/allowCors');
 require('dotenv').config();
 
 const app = express();
